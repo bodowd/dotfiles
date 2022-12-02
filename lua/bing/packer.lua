af5afe073f56ea3c659090f3783df8c9ce411d29
bodowd <odowd.bing@gmail.com>
2022-12-02 01:56:21 +0100

    add gitsigns

--- lua/bing/packer.lua
+++ lua/bing/packer.lua
@@ -41,4 +41,5 @@ return require('packer').startup(function(use)
     use 'nvim-telescope/telescope-file-browser.nvim'
     use 'kyazdani42/nvim-web-devicons' -- File icons for telescope browswer
 
+    use 'lewis6991/gitsigns.nvim'
 end)
