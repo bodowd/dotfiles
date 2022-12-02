6719fbdb851d8f7bb7f333309bf3356bbba38f77
giwds <bing.odowd@bayer.com>
2022-12-02 13:03:49 +0100

    Add command Wa to save all buffers and close them after for rename and also changed some mappings

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -7,8 +7,8 @@ saga.init_lsp_saga {
     },
     code_action_icon = "",
     finder_action_keys = {
-        open = 'o',
-        vsplit = '<CR>',
+        open = '<CR>',
+        vsplit = '<C-v>',
         split = 'i',
         tabe = 't',
         quit = { 'q', '<ESC>' },
@@ -23,5 +23,8 @@ vim.keymap.set('n', '<F12>', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>Lspsaga signature_help<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'rn', '<Cmd>Lspsaga rename<CR>', opts)
+-- when renaming across files, it will open it in buffers. Use :wa to save across all buffers
+-- %bd|e# will close the open buffers where the renamed files are
+vim.api.nvim_create_user_command('Wa', 'wa|%bd|e#', { nargs = 0 })
 vim.keymap.set('n', '~', '<Cmd>Lspsaga open_floaterm<CR>', opts)
 vim.keymap.set('t', '~', '<Cmd>Lspsaga close_floaterm<CR>', opts)
