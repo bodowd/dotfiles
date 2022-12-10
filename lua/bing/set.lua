c1b6f24e43ce0ae60161eec3df24169da368ab3a
bodowd <odowd.bing@gmail.com>
2022-12-10 15:39:28 +0100

    Turn off lspsaga floatterm and switch to nvim terminal mode
    The floatterm was having problems when I scroll to past lines of text
    and wasn't able to quit if scrolling. Nvim terminal mode doesn't seem
    to have this problem...yet...

--- lua/bing/set.lua
+++ lua/bing/set.lua
@@ -64,9 +64,9 @@ vim.cmd("nnoremap <leader>4 4gt")
 vim.cmd("nnoremap H :tabprevious<CR>")
 vim.cmd("nnoremap L :tabnext<CR>")
 
----- exit terminal editing mode to get to vim normal mode
---vim.cmd("tnoremap  <Esc> <C-\\><C-n>")
----- open terminal in new tab
---vim.keymap.set('n', '~', '<Cmd>tabnew | term<CR>')
----- close terminal when in terminal mode and close the buffer
---vim.keymap.set('t', '~', '<Cmd>bd!<CR>')
+-- exit terminal editing mode to get to vim normal mode
+vim.cmd("tnoremap  <Esc> <C-\\><C-n>")
+-- open terminal in new tab
+vim.keymap.set('n', '~', '<Cmd>tabnew | term<CR>')
+-- close terminal when in terminal mode and close the buffer
+vim.keymap.set('t', '~', '<Cmd>bd!<CR>')
--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -25,6 +25,6 @@ vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'rn', '<Cmd>Lspsaga rename<CR>', opts)
 -- when renaming across files, it will open it in buffers. Use :wa to save across all buffers
 -- %bd|e# will close the open buffers where the renamed files are
-vim.api.nvim_create_user_command('Wa', 'wa|%bd|e#', { nargs = 0 })
-vim.keymap.set('n', '~', '<Cmd>Lspsaga open_floaterm<CR>', opts)
-vim.keymap.set('t', '~', '<Cmd>Lspsaga close_floaterm<CR>', opts)
+-- vim.api.nvim_create_user_command('Wa', 'wa|%bd|e#', { nargs = 0 })
+-- vim.keymap.set('n', '~', '<Cmd>Lspsaga open_floaterm<CR>', opts)
+-- vim.keymap.set('t', '~', '<Cmd>Lspsaga close_floaterm<CR>', opts)
