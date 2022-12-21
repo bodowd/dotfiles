dc121ff2eb3f6ebddfb300c7f31c92a69707a0a5
giwds <bing.odowd@bayer.com>
2022-12-21 15:42:48 +0100

    Add border color to window split

--- after/plugin/tokyonight.rc.lua
+++ after/plugin/tokyonight.rc.lua
@@ -21,6 +21,9 @@ require("tokyonight").setup({
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
     dim_inactive = false, -- dims inactive windows
     lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+    on_colors = function(colors)
+    colors.border ='#565f89'
+    end
 
 })
 
