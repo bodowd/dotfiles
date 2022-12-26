6049a187c13cdaf45fa83714c96d8381fe52caa2
bodowd <odowd.bing@gmail.com>
2022-12-26 17:03:55 +0100

    Add keymap to see git diff

--- after/plugin/gitsigns.rc.lua
+++ after/plugin/gitsigns.rc.lua
@@ -1,4 +1,13 @@
 local status, gitsigns = pcall(require, "gitsigns")
 if (not status) then return end
 
-gitsigns.setup {}
+gitsigns.setup {
+    on_attach=function(bufnr)
+        local gs = package.loaded.gitsigns
+        -- local function map(mode, l, r, opts)
+        -- end
+        local opts = {buffer=bufnr}
+        vim.keymap.set('n', '<leader>gd', gs.diffthis, opts)
+
+    end
+}
