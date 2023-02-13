c222330abb79fca9ee3088499158c8fbd9326758
giwds <bing.odowd@bayer.com>
2023-02-13 11:22:01 +0100

    Add moonbow theme

--- after/plugin/tokyonight.rc.lua
+++ after/plugin/tokyonight.rc.lua
@@ -30,4 +30,4 @@ require("tokyonight").setup({
 
 
 
-vim.cmd [[colorscheme tokyonight-night]]
+-- vim.cmd [[colorscheme tokyonight-night]]
--- lua/bing/packer.lua
+++ lua/bing/packer.lua
@@ -2,71 +2,76 @@
 --vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+        -- Packer can manage itself
+        use 'wbthomason/packer.nvim'
 
-    -- colorthemes
-    use 'folke/tokyonight.nvim'
-    -- use 'tjdevries/colorbuddy.nvim'
-    --    use {
-    --        'svrana/neosolarized.nvim',
-    --        requires = { 'tjdevries/colorbuddy.nvim' }
-    --    }
+        -- colorthemes
+        use 'folke/tokyonight.nvim'
+        use "arturgoms/moonbow.nvim"
+        -- use 'tjdevries/colorbuddy.nvim'
+        --    use {
+        --        'svrana/neosolarized.nvim',
+        --        requires = { 'tjdevries/colorbuddy.nvim' }
+        --    }
 
-    use 'feline-nvim/feline.nvim' -- Status line
+        use 'feline-nvim/feline.nvim' -- Status line
 
-    use 'nvim-lua/plenary.nvim' -- Common utilities
+        use 'nvim-lua/plenary.nvim' -- Common utilities
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+        use {
+            'VonHeikemen/lsp-zero.nvim',
+            requires = {
+                -- LSP Support
+                { 'neovim/nvim-lspconfig' },
+                { 'williamboman/mason.nvim' },
+                { 'williamboman/mason-lspconfig.nvim' },
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+                -- Autocompletion
+                { 'hrsh7th/nvim-cmp' },
+                { 'hrsh7th/cmp-buffer' },
+                { 'hrsh7th/cmp-path' },
+                { 'saadparwaiz1/cmp_luasnip' },
+                { 'hrsh7th/cmp-nvim-lsp' },
+                { 'hrsh7th/cmp-nvim-lua' },
 
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
+                -- Snippets
+                { 'L3MON4D3/LuaSnip' },
+                { 'rafamadriz/friendly-snippets' },
+            }
         }
-    }
 
-    use 'jose-elias-alvarez/null-ls.nvim' -- Use nvim as a language server to inject LSP diagnostics, code actions, and more via Lua
+        use 'jose-elias-alvarez/null-ls.nvim' -- Use nvim as a language server to inject LSP diagnostics, code actions, and more via Lua
 
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-    use 'windwp/nvim-autopairs'
-    use 'windwp/nvim-ts-autotag'
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = ':TSUpdate'
+        }
+        use 'windwp/nvim-autopairs'
+        use 'windwp/nvim-ts-autotag'
 
-    use 'nvim-telescope/telescope.nvim'
-    use 'nvim-telescope/telescope-file-browser.nvim'
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        },
-        tag = 'nightly' -- optional, updated every week. (see issue #1193)
-    }
+        use 'nvim-telescope/telescope.nvim'
+        use 'nvim-telescope/telescope-file-browser.nvim'
+        use {
+            'nvim-tree/nvim-tree.lua',
+            requires = {
+                'nvim-tree/nvim-web-devicons', -- optional, for file icons
+            },
+            tag = 'nightly' -- optional, updated every week. (see issue #1193)
+        }
 
-    use 'ThePrimeagen/harpoon'
+        use 'ThePrimeagen/harpoon'
 
-    use 'kyazdani42/nvim-web-devicons' -- File icons for telescope browswer
-    use 'onsails/lspkind-nvim' -- VSCode like pictograms
+        use 'kyazdani42/nvim-web-devicons' -- File icons for telescope browswer
+        use 'onsails/lspkind-nvim' -- VSCode like pictograms
 
-    use 'lewis6991/gitsigns.nvim'
-    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
-        setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+        use 'lewis6991/gitsigns.nvim'
+        use({
+            "iamcco/markdown-preview.nvim",
+            run = "cd app && npm install",
+            setup = function() vim.g.mkdp_filetypes = { "markdown" } end,
+            ft = { "markdown" },
+        })
 
-    use 'numToStr/Comment.nvim'
-    use { "akinsho/toggleterm.nvim", tag = '*' }
-end)
+        use 'numToStr/Comment.nvim'
+        use { "akinsho/toggleterm.nvim", tag = '*' }
+    end)
