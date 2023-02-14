010ee4ce345985194581e8628b4224f5df8a6293
giwds <bing.odowd@bayer.com>
2023-02-14 16:09:13 +0100

    Change git branch logo

--- after/plugin/lualine.rc.lua
+++ after/plugin/lualine.rc.lua
@@ -4,5 +4,18 @@ require('lualine').setup {
         section_separators = '',
         component_separators = '',
         disabled_filetypes = { 'NvimTree', 'toggleterm' }
-    }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+            {
+                'branch',
+                icon = '󰊢'
+            },
+            'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 }
