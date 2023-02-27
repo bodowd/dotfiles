3b2308d1f9372db7672ee066fd69d4aa58c2e3fe
giwds <bing.odowd@bayer.com>
2023-02-27 09:15:49 +0100

    Increase window size of toggleterm

--- after/plugin/toggleterm.rc.lua
+++ after/plugin/toggleterm.rc.lua
@@ -1,7 +1,9 @@
 local status, tt = pcall(require, "toggleterm")
 if (not status) then return end
 
-tt.setup()
+tt.setup{
+    size=25
+}
 
 vim.keymap.set('n', '~', '<CMD>ToggleTerm<CR>')
 vim.keymap.set('t', '~', '<CMD>ToggleTerm<CR>')
