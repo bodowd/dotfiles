c97e12c8f04fc3f032748f0ef7e4883bf858d414
bodowd <odowd.bing@gmail.com>
2023-07-28 22:48:32 +0200

    Add symlink to kitty binary

--- mklinks.sh
+++ mklinks.sh
@@ -1,3 +1,5 @@
 ln -sfn ~/dotfiles/nvim ~/.config/nvim
 ln -sfn ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+# create symlink to the binary so that it can be run in any terminal
+sudo ln -sfn ~/.local/kitty.app/bin/kitty /usr/local/bin/
 ln -sfn ~/dotfiles/kitty/ ~/.config/kitty
