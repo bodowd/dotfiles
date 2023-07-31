19f0dd00dc64e0f84c66b8ca8d9473170d3b1556
giwds <bing.odowd@bayer.com>
2023-07-31 10:09:04 +0200

    Add check for OS to make link only for linux
    On mac, kitty is installed with homebrew

--- mklinks.sh
+++ mklinks.sh
@@ -1,5 +1,9 @@
 ln -sfn ~/dotfiles/nvim ~/.config/nvim
 ln -sfn ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 # create symlink to the binary so that it can be run in any terminal
-sudo ln -sfn ~/.local/kitty.app/bin/kitty /usr/local/bin/
+if [[ $(uname -s) = "Linux" ]]; then
+    echo "Detected Linux"
+    echo "linking kitty.app to /usr/local/bin"
+    sudo ln -sfn ~/.local/kitty.app/bin/kitty /usr/local/bin/
+fi
 ln -sfn ~/dotfiles/kitty/ ~/.config/kitty
