6acc15b55a1c5b1ca878d82657c44b4a9efb758b
bodowd <odowd.bing@gmail.com>
2024-05-01 22:21:39 +0200

    display full path in lualine

--- nvim/after/plugin/lualine.rc.lua
+++ nvim/after/plugin/lualine.rc.lua
@@ -15,7 +15,7 @@ require("lualine").setup({
 			"diff",
 			"diagnostics",
 		},
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
