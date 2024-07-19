13601f7eec99a06352107a72100e22eaee658ae7
bingodowd <bing.odowd@bayer.com>
2024-07-19 15:00:22 +0200

    update nvimtree

--- nvim/after/plugin/nvimtree.rc.lua
+++ nvim/after/plugin/nvimtree.rc.lua
@@ -7,19 +7,26 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
+local function my_on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	-- default mappings
+	api.config.mappings.default_on_attach(bufnr)
+
+	-- custom mappings
+	vim.keymap.set("n", "N", api.fs.create, opts("Create File Or Directory"))
+end
 -- OR setup with some options
 require("nvim-tree").setup({
 	sort_by = "case_sensitive",
+	on_attach = my_on_attach,
 	view = {
 		relativenumber = true,
 		adaptive_size = true,
-		mappings = {
-			list = {
-				{ key = "<C-s>", action = "split" },
-				{ key = "N", action = "create" },
-				{ key = "H", action = "" },
-			},
-		},
 	},
 	renderer = {
 		group_empty = true,
@@ -35,5 +42,3 @@ require("nvim-tree").setup({
 		enable = true,
 	},
 })
-
-vim.keymap.set("n", "<leader>t", "<Cmd>NvimTreeToggle<CR>")
