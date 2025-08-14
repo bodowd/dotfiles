c0875cd59c11dbeb442de825aa5964e037ea8036
bingodowd <bing.odowd@bayer.com>
2025-08-15 00:19:12 +0200

    add key map for navigation and moving lines

--- nvim/init.lua
+++ nvim/init.lua
@@ -187,6 +187,13 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- move highlighted lines up and down
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+vim.cmd 'nnoremap H :tabprevious<CR>'
+vim.cmd 'nnoremap L :tabnext<CR>'
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
